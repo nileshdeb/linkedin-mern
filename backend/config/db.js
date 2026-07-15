93ae5eaa558b81a59b91e2fb790ee76c15d2cc1b
Nilesh Deb <debnilesh69@gmail.com>
2026-07-15 23:13:48 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDb = async ()=>{
+    try{
+        mongoose.connect(process.env.MONGODB_URI)
+        console.log("db connected")
+
+    }catch(err){
+        console.log("db error");
+    }
+}
+
+export default connectDb; 
---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from'dotenv';
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+import connectDb from './config/db.js';
+dotenv.config();
+
+
+let app =express();
+let port =process.env.PORT || 5000
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(port,()=>{
+    connectDb();
+    console.log("server started")
+})
