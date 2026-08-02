dc325b66ebfcb8b5770d6f8e40056dca9404a0ab
Nilesh Deb <debnilesh69@gmail.com>
2026-08-02 17:25:19 +0530

    added login and logout feature

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -49,3 +49,46 @@ export const signUp=async(req,res)=>{
 
      }
 }
+
+export const login = async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        let user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"user does not exist!"})
+        }
+        const isMatch =await bcrypt.compare(password,user.password)
+        if(!isMatch){
+             return res.status(400).json({message:"invalid credentials!"})
+        }
+
+        let token= await genToken(user._id)
+        res.cookie("token",token,{
+            httpOnly:true,
+            maxAge:7*24*60*60*1000,
+            sameSite:"strict",
+            secure:process.env.NODE_ENVIRONMENT==="production"?true:false
+        })
+
+        return res.status(200).json(user)
+
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"login error"})
+    }
+}
+
+export const logOut =async(req,res)=>{
+    try{
+        res.clearCookie('token')
+        return res.status(200).json({message:"log out successfully"})
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"logout error"})
+
+    }
+
+
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { signUp } from "../controllers/auth.controllers.js";
+import { signUp,login, logOut } from "../controllers/auth.controllers.js";
 
 let authRouter=express.Router();
 
 
 authRouter.post("/signup",signUp);
+authRouter.post("/login",login);
+authRouter.get("/logout",logOut);
+
+
 
 export default authRouter;
