c2845b32f0763553bffb7288e81705034dabac43
Nilesh Deb <debnilesh69@gmail.com>
2026-08-05 22:58:52 +0530

    created api call function from client signup page  to server

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,24 +1,51 @@
-import {React,useState} from 'react'
+import {React,useState,useContext} from 'react'
 import logo from "../assets/logo.svg"
 import {useNavigate} from "react-router-dom"
+import { authDataContext } from '../context/AuthContext'
+import axios from 'axios'
 function Signup() {
   let [show,setShow]=useState(false)
   let navigate=useNavigate()
+  let {serverUrl}=useContext(authDataContext)
+
+  let [firstName,setFirstName]=useState("")
+  let [lastName,setLastName]=useState("")
+  let [userName,setUserName]=useState("")
+  let [email,setEmail]=useState("")
+  let [password,setPassword]=useState("")
+
+  const handleSignup=async (e)=>{
+    e.preventDefault()
+    try{
+      let result = await axios.post(serverUrl+"/api/auth/signup",{
+        firstName,
+        lastName,
+        userName,
+        email,
+        password
+
+      },{withCredentials:true})
+      console.log(result)
+
+    }catch(error){
+
+    }
+  }
 
   return (
     <div className='w-full h-screen bg-[white] flex flex-col items-center justify-start'>
       <div className='pt-[10px] pb-[30px] px-[30px] lg:pt-[15px] lg:pb-[35px] lg:px-[35px] w-full'>
         <img src={logo} alt="LinkedIn" className='w-[135px]' />
       </div>
-      <form className='w-[90%] max-w-[400px] h-[600px] md:shadow-xl flex flex-col justify-center gap-[10px] p-[15px]'>
+      <form className='w-[90%] max-w-[400px] h-[600px] md:shadow-xl flex flex-col justify-center gap-[10px] p-[15px]' onSubmit={handleSignup}>
         <h1 className='text-gray-800 text-[30px] font-semibold mb-[30px]'>Sign Up</h1>
-        <input type="text" placeholder='firstname' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px]  px-[20px] py-[10px] rounded-md' />
-        <input type="text" placeholder='lastname' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
-        <input type="text" placeholder='username' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
-        <input type="email" placeholder='email' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
+        <input type="text" placeholder='firstname' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px]  px-[20px] py-[10px] rounded-md' value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+        <input type="text" placeholder='lastname' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+        <input type="text" placeholder='username' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' value={userName} onChange={(e)=>setUserName(e.target.value)}/>
+        <input type="email" placeholder='email' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' value={email} onChange={(e)=>setEmail(e.target.value)}/>
 
         <div  className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px]  rounded-md relative '>
-          <input type={show?"text":"password"} placeholder='password' required className='w-full h-full border-none text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
+          <input type={show?"text":"password"} placeholder='password' required className='w-full h-full border-none text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' value={password} onChange={(e)=>setPassword(e.target.value)} />
           <span className='absolute right-[20px] top-[10px] text-[#24b2ff] cursor-pointer font-semibold ' onClick={()=>setShow(prev=>!prev)}>{show?"hidden":"show"}</span>
         </div>
         
