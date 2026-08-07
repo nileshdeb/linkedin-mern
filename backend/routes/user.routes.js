31861998eeab072655faecdeb91e8c9461782a4f
Nilesh Deb <debnilesh69@gmail.com>
2026-08-07 23:01:24 +0530

    backend/controllers/user.controllers.js         backend/middlewares/         backend/routes/user.routes.js

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getCurrentUser } from "../controllers/user.controllers.js";
+import isAuth from "../middlewares/isAuth.js";
+
+let userRouter=express.Router()
+
+userRouter.get('/currentuser',isAuth,getCurrentUser)
+
+export default userRouter;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,14 +13,11 @@ function login() {
   let [loading, setLoading] = useState(false)
   let [err, setErr] = useState("")
 
-  const handleSignup = async (e) => {
+  const handleSignIn = async (e) => {
     e.preventDefault()
     setLoading(true)
     try {
-      let result = await axios.post(serverUrl + "/api/auth/signup", {
-        firstName,
-        lastName,
-        userName,
+      let result = await axios.post(serverUrl + "/api/auth/login", {
         email,
         password
 
@@ -28,9 +25,6 @@ function login() {
       console.log(result)
       setErr("")
       setLoading(false)
-      setFirstName("")
-      setLastName("")
-      setUserName("")
       setEmail("")
       setPassword("")
 
@@ -45,7 +39,7 @@ function login() {
       <div className='pt-[10px] pb-[30px] px-[30px] lg:pt-[15px] lg:pb-[35px] lg:px-[35px] w-full'>
         <img src={logo} alt="LinkedIn" className='w-[135px]' />
       </div>
-      <form className='w-[90%] max-w-[400px] h-[600px] md:shadow-xl flex flex-col justify-center gap-[10px] p-[15px]' onSubmit={handleSignup}>
+      <form className='w-[90%] max-w-[400px] h-[600px] md:shadow-xl flex flex-col justify-center gap-[10px] p-[15px]' onSubmit={handleSignIn}>
         <h1 className='text-gray-800 text-[30px] font-semibold mb-[30px]'>Sign In</h1>
 
         <input type="email" placeholder='email' required className='w-[100%] h-[50px] border-2 border-gray-600  text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' value={email} onChange={(e) => setEmail(e.target.value)} />
